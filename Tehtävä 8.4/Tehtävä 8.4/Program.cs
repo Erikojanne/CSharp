853af68f1e57172f513b521036b5f897cbe1d02d
Erikojanne <79418401+Erikojanne@users.noreply.github.com>
2022-03-31 12:08:57 +0300

    Tehtävä 8.4

---
+++ Tehtävä 8.4/Tehtävä 8.4/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Tehtävä 8.4 ");
+Console.WriteLine();
+Console.WriteLine("--------------------");
+Console.WriteLine();
+Console.Write("HEROt ovat seuraavat :");
+Console.WriteLine();
+int[] luku = { 1, 2, 3, 4};
+string[] nimet = {"Ana", "Bastion", "Brigette", "D.Va(mech)"};
+
+
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine( + luku[i] + " " + nimet[i]);
+   
+}
+Console.WriteLine();
+//int numero;
+
+/*Console.Write("Valitse minkä HEROn tiedot haluat nähdä ? ");
+int numero = int.Parse(Console.ReadLine());
+
+string[] nimi = new string[4];
+int[] luvut = new int[4];
+//nimi[0] = 
+//int[] lkm = new int[numero]; // luodaan taulukko
+
+
+
+
+/*for (int i = 0; i < numero; i++)
+{
+    Console.Write("Anna {0}. tuotteen nimi ? ", i + 1);
+    nimi[i] = Console.ReadLine();
+    Console.Write("Anna {0}. tuotteen hinta ? ", i + 1);
+    hinta[i] = int.Parse(Console.ReadLine());
+    Console.Write("Anna {0}. tuotteen lukumäärä ? ", i + 1);
+    lkm[i] = int.Parse(Console.ReadLine());
+    int tulo = hinta[i] * lkm[i];
+    Console.WriteLine();
+    Console.WriteLine(nimi[i] + " " + tulo + " € ");
+
+}*/
+
+
+
+
+
