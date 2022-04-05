efe33059a1eb2761f5ca8fd6c12a170e29ac6df7
Erikojanne <79418401+Erikojanne@users.noreply.github.com>
2022-04-05 12:52:09 +0300

    5.4.2022

--- Tehtävä 8.4/Tehtävä 8.4/Program.cs
+++ Tehtävä 8.4/Tehtävä 8.4/Program.cs
@@ -5,44 +5,30 @@ Console.WriteLine("Tehtävä 8.4 ");
 Console.WriteLine();
 Console.WriteLine("--------------------");
 Console.WriteLine();
-Console.Write("HEROt ovat seuraavat :");
-Console.WriteLine();
-int[] luku = { 1, 2, 3, 4};
-string[] nimet = {"Ana", "Bastion", "Brigette", "D.Va(mech)"};
+int Hero;
+string[,] nimet = { { ("HERO"), ("Ana"), ("Bastion"), ("Brigette"), ("D.Va (Mech)") },
+                  { ("ROLE"), ("Support"), ("Defence"), ("Support"), ("Tank")},
+                  { ("HEALTH"), ("200"), ("200"), ("200"), ("400")},
+                  { ("AMOR"), ("0"), ("100"), ("50"), ("200") } };
 
+Console.WriteLine("HEROt ovat seuraavat :");
+Console.WriteLine("(1) Ana ");
+Console.WriteLine("(2) Bastion ");
+Console.WriteLine("(3) Brigette ");
+Console.WriteLine("(4) D.Va (Mech ");
+Console.WriteLine();
+Console.Write("Valitse minkä HEROn tiedot haluat nähdä ? ");
+
+Hero = int.Parse(Console.ReadLine());
+nimet =  Console.ReadLine());
+
+Console.WriteLine();
 
 for (int i = 0; i < 4; i++)
 {
-    Console.WriteLine( + luku[i] + " " + nimet[i]);
-   
+    Console.WriteLine(Hero + " " + 2, Hero + " " + 3, Hero + " " + 4, Hero);
 }
-Console.WriteLine();
-//int numero;
 
-/*Console.Write("Valitse minkä HEROn tiedot haluat nähdä ? ");
-int numero = int.Parse(Console.ReadLine());
-
-string[] nimi = new string[4];
-int[] luvut = new int[4];
-//nimi[0] = 
-//int[] lkm = new int[numero]; // luodaan taulukko
-
-
-
-
-/*for (int i = 0; i < numero; i++)
-{
-    Console.Write("Anna {0}. tuotteen nimi ? ", i + 1);
-    nimi[i] = Console.ReadLine();
-    Console.Write("Anna {0}. tuotteen hinta ? ", i + 1);
-    hinta[i] = int.Parse(Console.ReadLine());
-    Console.Write("Anna {0}. tuotteen lukumäärä ? ", i + 1);
-    lkm[i] = int.Parse(Console.ReadLine());
-    int tulo = hinta[i] * lkm[i];
-    Console.WriteLine();
-    Console.WriteLine(nimi[i] + " " + tulo + " € ");
-
-}*/
 
 
 
